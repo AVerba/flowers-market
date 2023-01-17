645f88ab2be79202f2902d4127d4b859e0b0aabe
AVerba <57949955+AVerba@users.noreply.github.com>
2023-01-17 17:24:03 +0000

    Deploying to gh-pages from @ AVerba/flowers-market@e972d936e2ab4e7b0bf2b3ba03e29cb70a3abacb 🚀

---
+++ index.4bbaea0d.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".ac-item__header").forEach((function(e){e.addEventListener("click",(function(t){var c=e.querySelector(".ac-item__icon");console.log(c),t.currentTarget.parentElement.querySelector(".ac-item__description").classList.toggle("descrip-active"),t.currentTarget.parentElement.classList.toggle("_accord-active")}))}));
+//# sourceMappingURL=index.4bbaea0d.js.map
