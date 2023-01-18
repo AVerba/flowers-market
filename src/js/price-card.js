37308e8bb366604f66f896676c3fea17cc2d9f5b
Oleksii Verbytskyi <alverba88@gmail.com>
2023-01-18 16:22:08 +0200

    mod select form

--- src/js/price-card.js
+++ src/js/price-card.js
@@ -4,8 +4,6 @@ const collapsButtons = document.querySelectorAll(".ac-item__header");
 collapsButtons.forEach((collapsBtn)=>{
 
   collapsBtn.addEventListener('click',(event)=>{
-    const Btn=collapsBtn.querySelector(".ac-item__icon");
-    console.log(Btn)
     event.currentTarget.parentElement.querySelector(".ac-item__description").classList.toggle("descrip-active");
     event.currentTarget.parentElement.classList.toggle("_accord-active");
   });
